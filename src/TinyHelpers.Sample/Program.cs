e5e717013e0dc0ceb6a4ca6db88858997efe244f
Marco Minerva <marco.minerva@gmail.com>
2021-05-09 17:22:04 +0200

    Add converter to handle TimeSpan as ticks

--- src/TinyHelpers.Sample/Program.cs
+++ src/TinyHelpers.Sample/Program.cs
@@ -3,7 +3,6 @@ using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Globalization;
 using System.Runtime.Serialization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -12,133 +11,128 @@ using TinyHelpers.Extensions;
 using TinyHelpers.Json.Serialization;
 using TinyHelpers.Threading;
 
-namespace TinyHelpers.Sample
+var dateTime = new DateTime(2020, 1, 1);
+var test = TimeSpan.Parse("1.14:30:16");
+
+
+var list = new Dictionary<string, string> { ["A"] = "First Value", ["B"] = "Second Value" };
+
+var jsonSerializerSettings = new JsonSerializerSettings
 {
-    public class Person
-    {
-        [JsonPropertyName("first_name")]
-        public string? FirstName { get; set; }
+    DateTimeZoneHandling = DateTimeZoneHandling.Utc
+};
 
-        public string? LastName { get; set; }
-    }
-
-    public enum Priority
-    {
-        [Display(Name = "Bassa")]
-        [EnumMember(Value = "Bassa priorita")]
-        Low,
-
-        [Display(Name = "Media")]
-        [EnumMember(Value = "Media priorita")]
-        Medium,
-
-        [Display(Name = "Alta")]
-        [EnumMember(Value = "Alta priorita")]
-        High
-    };
-
-    [Flags]
-    public enum ConnectionTypes
-    {
-        Wired = 1,
-        WiFi = 2,
-        Bluetooth = 4,
-        [Display(Name = "Via Satellite")]
-        Satellite = 8
-    };
-
-    internal class Program
-    {
-        private static readonly AsyncLock syncObject = new AsyncLock();
-
-        private static async Task Main(string[] args)
-        {
-            var dateTime = new DateTime(2020, 1, 1);
-
-            var test = TimeSpan.Parse("1:14:30:15");
-
-            var str = test.ToString(null, CultureInfo.InvariantCulture);
-
-            var list = new Dictionary<string, string> { ["A"] = "First Value", ["B"] = "Second Value" };
-
-            var jsonSerializerSettings = new JsonSerializerSettings
-            {
-                DateTimeZoneHandling = DateTimeZoneHandling.Utc
-            };
-
-            jsonSerializerSettings.Converters.Add(new StringEnumConverter());
+jsonSerializerSettings.Converters.Add(new StringEnumConverter());
 
 
-            var time = new TimeSpan(25, 42, 36);
+var time = new TimeSpan(25, 42, 36);
 
-            var oldJson = JsonConvert.SerializeObject(time, jsonSerializerSettings);
-            var oldResult = JsonConvert.DeserializeObject<TimeSpan>(oldJson, jsonSerializerSettings);
+var oldJson = JsonConvert.SerializeObject(time, jsonSerializerSettings);
+var oldResult = JsonConvert.DeserializeObject<TimeSpan>(oldJson, jsonSerializerSettings);
 
 
-            var jsonSerializerOptions = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true,
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            };
+var jsonSerializerOptions = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+};
 
-            jsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
-            jsonSerializerOptions.Converters.Add(new TimeSpanConverter());
-            jsonSerializerOptions.Converters.Add(new StringEnumMemberConverter());
+jsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
+jsonSerializerOptions.Converters.Add(new TimeSpanTicksConverter());
+jsonSerializerOptions.Converters.Add(new StringEnumMemberConverter());
 
-            var json = System.Text.Json.JsonSerializer.Serialize(test, jsonSerializerOptions);
-            var result = System.Text.Json.JsonSerializer.Deserialize<TimeSpan>(json, jsonSerializerOptions);
+var Test = new Test { Time = test };
 
-            var connectionTypes = ConnectionTypes.Wired | ConnectionTypes.Satellite;
-            var description = typeof(ConnectionTypes).GetDescriptions();
-            foreach (var connectionType in connectionTypes.GetFlags())
-            {
-                Foo(connectionType);
-            }
+var json = System.Text.Json.JsonSerializer.Serialize(Test, jsonSerializerOptions);
+var result = System.Text.Json.JsonSerializer.Deserialize<Test>(json, jsonSerializerOptions);
 
-            connectionTypes.GetFlags().ForEach(c => Foo(c));
+var connectionTypes = ConnectionTypes.Wired | ConnectionTypes.Satellite;
+var description = typeof(ConnectionTypes).GetDescriptions();
+foreach (var connectionType in connectionTypes.GetFlags())
+{
+    Foo(connectionType);
+}
 
-            var priority = Priority.Medium;
-            Console.WriteLine($"La priorità è {priority.GetDescription()}");
+connectionTypes.GetFlags().ForEach(c => Foo(c));
 
-            //var a = "Marco";
-            //var b = "marco";
+var priority = Priority.Medium;
+Console.WriteLine($"La priorità è {priority.GetDescription()}");
 
-            //if (a.EqualsIgnoreCase(b))
-            //{
+//var a = "Marco";
+//var b = "marco";
 
-            //}
+//if (a.EqualsIgnoreCase(b))
+//{
 
-            //var inputName = "";
-            //var name = inputName.GetValueOrDefault("sconosciuto");
+//}
 
-            var people = GetPeople();
+//var inputName = "";
+//var name = inputName.GetValueOrDefault("sconosciuto");
 
-            foreach (var (person, i) in people.WithIndex())
-            {
-                Console.WriteLine($"Elaboro la persona {i}...");
-            }
+var people = GetPeople();
 
-            var task = Task.Delay(5000);
-            await task.TimeoutAfterAsync(TimeSpan.FromSeconds(10));
+foreach (var (person, i) in people.WithIndex())
+{
+    Console.WriteLine($"Elaboro la persona {i}...");
+}
 
-            using (await syncObject.LockAsync())
-            {
-                await Task.Delay(1000);
-            }
-        }
+var task = Task.Delay(5000);
+await task.TimeoutAfterAsync(TimeSpan.FromSeconds(10));
 
-        private static void Foo(ConnectionTypes connectionType) => Console.WriteLine($"Analizzo la connessione {connectionType.ToString()}...");// ...
+var syncObject = new AsyncLock();
+using (await syncObject.LockAsync())
+{
+    await Task.Delay(1000);
+}
 
-        private static IEnumerable<Person> GetPeople()
-        {
-            var people = new List<Person>
+static void Foo(ConnectionTypes connectionType) => Console.WriteLine($"Analizzo la connessione {connectionType.ToString()}...");// ...
+
+static IEnumerable<Person> GetPeople()
+{
+    var people = new List<Person>
            {
                 new Person { FirstName = "Donald", LastName = "Duck" },
                 new Person { FirstName = "Mickey", LastName = "Mouse" },
                 new Person { FirstName = "Daisy", LastName = "Duck" }
              };
 
-            return people;
-        }
-    }
+    return people;
 }
+
+public class Test
+{
+    public TimeSpan Time { get; set; }
+}
+
+public class Person
+{
+    [JsonPropertyName("first_name")]
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+}
+
+public enum Priority
+{
+    [Display(Name = "Bassa")]
+    [EnumMember(Value = "Bassa priorita")]
+    Low,
+
+    [Display(Name = "Media")]
+    [EnumMember(Value = "Media priorita")]
+    Medium,
+
+    [Display(Name = "Alta")]
+    [EnumMember(Value = "Alta priorita")]
+    High
+};
+
+[Flags]
+public enum ConnectionTypes
+{
+    Wired = 1,
+    WiFi = 2,
+    Bluetooth = 4,
+    [Display(Name = "Via Satellite")]
+    Satellite = 8
+};
---
+++ src/TinyHelpers/Json/Serialization/TimeSpanTicksConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TinyHelpers.Json.Serialization
+{
+    public class TimeSpanTicksConverter : JsonConverter<TimeSpan>
+    {
+        public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            => TimeSpan.FromTicks(reader.GetInt64());
+
+        public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
+            => writer.WriteNumberValue(value.Ticks);
+    }
+}
