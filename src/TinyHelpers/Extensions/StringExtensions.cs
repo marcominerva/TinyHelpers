1b05fa1d7570e25632590137edfd922f3fe6f5e8
Marco Minerva <marco.minerva@gmail.com>
2021-09-10 17:24:24 +0200

    Fix null reference for parameters

--- src/TinyHelpers/Extensions/StringExtensions.cs
+++ src/TinyHelpers/Extensions/StringExtensions.cs
@@ -7,13 +7,13 @@ namespace TinyHelpers.Extensions
         public static bool EqualsIgnoreCase(this string? a, string? b)
             => string.Equals(a, b, StringComparison.OrdinalIgnoreCase);
 
-        public static bool StartsWithIgnoreCase(this string? a, string? b)
+        public static bool StartsWithIgnoreCase(this string? a, string b)
             => a?.StartsWith(b, StringComparison.OrdinalIgnoreCase) ?? false;
 
-        public static bool EndsWithIgnoreCase(this string? a, string? b)
+        public static bool EndsWithIgnoreCase(this string? a, string b)
             => a?.EndsWith(b, StringComparison.OrdinalIgnoreCase) ?? false;
 
-        public static bool ContainsIgnoreCase(this string? a, string? b)
+        public static bool ContainsIgnoreCase(this string? a, string b)
             => a?.IndexOf(b, StringComparison.OrdinalIgnoreCase) >= 0;
 
         public static string? GetValueOrDefault(this string? input, string? defaultValue = default, bool whitespaceAsEmpty = true)
