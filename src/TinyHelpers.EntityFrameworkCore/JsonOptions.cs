0afb2eaaf97549ac5cb061ff064b088db9aee384
Marco Minerva <marco.minerva@gmail.com>
2022-01-07 14:29:25 +0100

    Libraries update

--- src/TinyHelpers.EntityFrameworkCore/JsonOptions.cs
+++ src/TinyHelpers.EntityFrameworkCore/JsonOptions.cs
@@ -19,6 +19,5 @@ internal class JsonOptions
 
         Default.Converters.Add(new DateOnlyConverter());
         Default.Converters.Add(new TimeOnlyConverter());
-        Default.Converters.Add(new StringEnumMemberConverter());
     }
 }
