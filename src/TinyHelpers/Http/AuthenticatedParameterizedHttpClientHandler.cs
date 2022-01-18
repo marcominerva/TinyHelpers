c01bc659cb82da19f4abd8efcf7cbda664278e9c
Marco Minerva <marco.minerva@gmail.com>
2022-01-18 10:49:24 +0100

    Add parameter to skip Header check

--- src/TinyHelpers/Http/AuthenticatedParameterizedHttpClientHandler.cs
+++ src/TinyHelpers/Http/AuthenticatedParameterizedHttpClientHandler.cs
@@ -5,23 +5,31 @@ namespace TinyHelpers.Http;
 public class AuthenticatedParameterizedHttpClientHandler : DelegatingHandler
 {
     private readonly Func<HttpRequestMessage, Task<string>> getToken;
+    private readonly bool checkAuthorizationHeader;
 
-    public AuthenticatedParameterizedHttpClientHandler(Func<HttpRequestMessage, Task<string>> getToken)
+    public AuthenticatedParameterizedHttpClientHandler(Func<HttpRequestMessage, Task<string>> getToken, bool checkAuthorizationHeader = true)
     {
         this.getToken = getToken ?? throw new ArgumentNullException(nameof(getToken));
+        this.checkAuthorizationHeader = checkAuthorizationHeader;
     }
 
     public AuthenticatedParameterizedHttpClientHandler(Func<HttpRequestMessage, Task<string>> getToken, HttpMessageHandler innerHandler)
-        : base(innerHandler)
+        : this(getToken, true, innerHandler)
+    {
+    }
+
+    public AuthenticatedParameterizedHttpClientHandler(Func<HttpRequestMessage, Task<string>> getToken, bool checkAuthorizationHeader, HttpMessageHandler innerHandler)
+    : base(innerHandler)
     {
         this.getToken = getToken ?? throw new ArgumentNullException(nameof(getToken));
+        this.checkAuthorizationHeader = checkAuthorizationHeader;
     }
 
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         // See if the request has an authorize header
         var auth = request.Headers.Authorization;
-        if (auth is not null)
+        if (!checkAuthorizationHeader || request.Headers.Authorization is not null)
         {
             var token = await getToken(request).ConfigureAwait(false);
             request.Headers.Authorization = new AuthenticationHeaderValue(auth.Scheme, token);
