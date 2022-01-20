3ca98a57895a3295e5ee95b6c6b0bf5a579e3ec0
Marco Minerva <marco.minerva@gmail.com>
2022-01-20 10:03:20 +0100

    Add String empty to null converter

---
+++ src/TinyHelpers.EntityFrameworkCore/Converters/StringEmptyToNullConverter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace TinyHelpers.EntityFrameworkCore.Converters;
+
+public class StringEmptyToNullConverter : ValueConverter<string?, string?>
+{
+    public StringEmptyToNullConverter() : base(
+            value => string.IsNullOrWhiteSpace(value) ? null : value,
+            value => value)
+    {
+    }
+}
---
+++ src/TinyHelpers.EntityFrameworkCore/Converters/StringEmptyToNullTrimConverter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace TinyHelpers.EntityFrameworkCore.Converters;
+
+public class StringEmptyToNullTrimConverter : ValueConverter<string?, string?>
+{
+    public StringEmptyToNullTrimConverter() : base(
+            value => string.IsNullOrWhiteSpace(value) ? null : value.Trim(),
+            value => value)
+    {
+    }
+}
