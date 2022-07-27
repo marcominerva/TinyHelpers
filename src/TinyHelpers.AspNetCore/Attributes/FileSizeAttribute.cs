625314732bcb2fd5179fefc65f3d7d00b93757b4
albertodn <alberto.dallanora@telephonevox.com>
2022-07-27 22:13:36 +0200

    Add FileSize Validation Attribute

---
+++ src/TinyHelpers.AspNetCore/Attributes/FileSizeAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace TinyHelpers.AspNetCore.Attributes;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public class FileSizeAttribute : ValidationAttribute
+{
+    private readonly int maxFileSizeInMB;
+
+    public FileSizeAttribute(int maxFileSizeInMB)
+    {
+        this.maxFileSizeInMB = maxFileSizeInMB;
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value == null)
+        {
+            return ValidationResult.Success;
+        }
+
+        IFormFile? formFile = value as IFormFile;
+
+        if (formFile == null)
+        {
+            return ValidationResult.Success;
+        }
+
+        if (formFile.Length > maxFileSizeInMB * 1024 * 1024)
+        {
+            return new ValidationResult($"File size cannot be bigger than {maxFileSizeInMB} MB");
+        }
+
+        return ValidationResult.Success;
+    }
+}
