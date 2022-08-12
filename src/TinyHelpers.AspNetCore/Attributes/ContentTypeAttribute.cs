f51639a6ae6e3d371c2b9e03c85ac8c0fc150d0a
dack94 <davide.gagliardi@yahoo.it>
2022-08-12 22:17:51 +0200

    ContentTypeAttribute changed

--- src/TinyHelpers.AspNetCore/Attributes/ContentTypeAttribute.cs
+++ src/TinyHelpers.AspNetCore/Attributes/ContentTypeAttribute.cs
@@ -4,36 +4,48 @@ using Microsoft.AspNetCore.Http;
 
 namespace TinyHelpers.AspNetCore.Attributes;
 
+public enum FileType
+{
+    Image,
+    Video,
+    Audio
+}
+
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
 public class ContentTypeAttribute : ValidationAttribute
 {
-    private readonly string[] validContentType;
-    private readonly string[] imageContentTypes = new string[] { "image/jpeg", "image/png", "image/gif" };
-    private readonly string[] videoContentTypes = new string[] { "video/mp4", "video/ogg", "video/quicktime" };
+    private readonly string[] validContentTypes;
+    private readonly string[] imageContentTypes = new string[] { "image/jpeg", "image/png", "image/gif", "image/bmp", "image/cis-cod", "image/ief", "image/pipeg", "image/svg+xml", "image/tiff", "image/x-cmu-raster", "image/x-cmx", "image/x-icon", "image/x-portable-anymap", "image/x-portable-bitmap", "image/x-portable-graymap", "image/x-portable-pixmap", "image/x-rgb", "image/x-xbitmap", "image/x-xpixmap", "image/x-xwindowdump" };
+    private readonly string[] videoContentTypes = new string[] { "video/mp4", "video/ogg", "video/quicktime", "video/mpeg", "video/x-la-asf", "video/x-ms-asf", "video/x-msvideo", "video/x-sgi-movie" };
+    private readonly string[] audioContentTypes = new string[] { "audio/basic", "audio/mid", "audio/mpeg", "audio/x-wav", "audio/x-mpegurl", "audio/x-pn-realaudio" };
 
-    public ContentTypeAttribute(string[] ValidContentType)
+    public ContentTypeAttribute(params string[] validContentTypes)
+        : base("The {0} field should have one of the following content-types {1}")
     {
-        validContentType = ValidContentType;
+        this.validContentTypes = this.normalizeDatas(validContentTypes);
     }
 
-    public ContentTypeAttribute(ContentTypeGroup contentTypeGroup)
+    private string[] normalizeDatas(string[] validContentTypes)
+    {
+        return validContentTypes.Select(s => s.ToLowerInvariant()).ToArray();
+    }
+
+    public ContentTypeAttribute(FileType FileType)
         : base("The {0} field should have one of the following Content-Types {1}")
     {
-        switch (contentTypeGroup)
+
+        validContentTypes = FileType switch
         {
-            case ContentTypeGroup.Image:
-                validContentType = imageContentTypes;
-                break;
-            case ContentTypeGroup.Video:
-                validContentType = videoContentTypes;
-                break;
-            default: throw new System.ComponentModel.InvalidEnumArgumentException();
-        }
+            FileType.Image => imageContentTypes,
+            FileType.Video => videoContentTypes,
+            FileType.Audio => audioContentTypes,
+            _ => throw new System.ComponentModel.InvalidEnumArgumentException()
+        };
     }
 
-    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        if (value is IFormFile formFile && !validContentType.Contains(formFile.ContentType))
+        if (value is IFormFile formFile && !validContentTypes.Contains(formFile.ContentType))
         {
             return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
         }
@@ -42,11 +54,5 @@ public class ContentTypeAttribute : ValidationAttribute
     }
 
     public override string FormatErrorMessage(string name)
-        => string.Format(CultureInfo.CurrentCulture, ErrorMessageString, name, string.Join(", ", validContentType));
-}
-
-public enum ContentTypeGroup
-{
-    Image,
-    Video
+        => string.Format(CultureInfo.CurrentCulture, ErrorMessageString, name, string.Join(", ", validContentTypes));
 }
