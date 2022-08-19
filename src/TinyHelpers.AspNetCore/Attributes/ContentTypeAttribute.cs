dae704713d76fa5d681354241efd5d55bf153351
dack94 <davide.gagliardi@yahoo.it>
2022-08-19 22:54:09 +0200

    ContentTypeAttribute changed

--- src/TinyHelpers.AspNetCore/Attributes/ContentTypeAttribute.cs
+++ src/TinyHelpers.AspNetCore/Attributes/ContentTypeAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using Microsoft.AspNetCore.Http;
 
@@ -14,32 +15,29 @@ public enum FileType
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
 public class ContentTypeAttribute : ValidationAttribute
 {
-    private readonly string[] validContentTypes;
-    private readonly string[] imageContentTypes = new string[] { "image/jpeg", "image/png", "image/gif", "image/bmp", "image/cis-cod", "image/ief", "image/pipeg", "image/svg+xml", "image/tiff", "image/x-cmu-raster", "image/x-cmx", "image/x-icon", "image/x-portable-anymap", "image/x-portable-bitmap", "image/x-portable-graymap", "image/x-portable-pixmap", "image/x-rgb", "image/x-xbitmap", "image/x-xpixmap", "image/x-xwindowdump" };
-    private readonly string[] videoContentTypes = new string[] { "video/mp4", "video/ogg", "video/quicktime", "video/mpeg", "video/x-la-asf", "video/x-ms-asf", "video/x-msvideo", "video/x-sgi-movie" };
-    private readonly string[] audioContentTypes = new string[] { "audio/basic", "audio/mid", "audio/mpeg", "audio/x-wav", "audio/x-mpegurl", "audio/x-pn-realaudio" };
+    private readonly IEnumerable<string> validContentTypes;
+
+    private static readonly IEnumerable<string> imageContentTypes = new string[] { "image/jpeg", "image/png", "image/gif", "image/bmp", "image/cis-cod", "image/ief", "image/pipeg", "image/svg+xml", "image/tiff", "image/x-cmu-raster", "image/x-cmx", "image/x-icon", "image/x-portable-anymap", "image/x-portable-bitmap", "image/x-portable-graymap", "image/x-portable-pixmap", "image/x-rgb", "image/x-xbitmap", "image/x-xpixmap", "image/x-xwindowdump" };
+    private static readonly IEnumerable<string> videoContentTypes = new string[] { "video/mp4", "video/ogg", "video/quicktime", "video/mpeg", "video/x-la-asf", "video/x-ms-asf", "video/x-msvideo", "video/x-sgi-movie" };
+    private static readonly IEnumerable<string> audioContentTypes = new string[] { "audio/basic", "audio/mid", "audio/mpeg", "audio/x-wav", "audio/x-mpegurl", "audio/x-pn-realaudio" };
+
+    private const string DefaultErrorMessage = "The {0} field should have one of the following Content-Types: {1}";
 
     public ContentTypeAttribute(params string[] validContentTypes)
-        : base("The {0} field should have one of the following content-types {1}")
+        : base(DefaultErrorMessage)
     {
-        this.validContentTypes = this.normalizeDatas(validContentTypes);
-    }
-
-    private string[] normalizeDatas(string[] validContentTypes)
-    {
-        return validContentTypes.Select(s => s.ToLowerInvariant()).ToArray();
+        this.validContentTypes = validContentTypes.Select(s => s.ToLowerInvariant());
     }
 
     public ContentTypeAttribute(FileType FileType)
-        : base("The {0} field should have one of the following Content-Types {1}")
+        : base(DefaultErrorMessage)
     {
-
         validContentTypes = FileType switch
         {
             FileType.Image => imageContentTypes,
             FileType.Video => videoContentTypes,
             FileType.Audio => audioContentTypes,
-            _ => throw new System.ComponentModel.InvalidEnumArgumentException()
+            _ => throw new InvalidEnumArgumentException()
         };
     }
 
