e6bd2c9899fbb676285d4ae1dbe9ae7f33578425
Marco Minerva <marco.minerva@gmail.com>
2022-11-29 17:29:59 +0100

    Update to .NET 7.0 #90

--- src/TinyHelpers/Threading/AsyncLock.cs
+++ src/TinyHelpers/Threading/AsyncLock.cs
@@ -2,7 +2,7 @@
 
 public class AsyncLock : IDisposable
 {
-    private readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+    private readonly SemaphoreSlim semaphoreSlim = new(1, 1);
 
     public async Task<AsyncLock> LockAsync()
     {
