671ff4fc1232dcafff47b7d676a81216c81d7e2d
Marco Minerva <marco.minerva@gmail.com>
2022-11-30 12:36:30 +0100

    Update to .NET 7.0 #124 Add DateOnly/TimeOnly support #120

--- samples/TinyHelpers.Dapper.Sample/Program.cs
+++ samples/TinyHelpers.Dapper.Sample/Program.cs
@@ -1,28 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using Dapper;
 using Microsoft.Data.SqlClient;
-using TinyHelpers.Dapper.Sample.Models;
 using TinyHelpers.Dapper.TypeHandlers;
 
-const string ConnectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=Sample;Integrated Security=True";
+const string ConnectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=MinimalDB;Integrated Security=True";
 
 using var connection = new SqlConnection(ConnectionString);
-JsonTypeHandler<IList<Review>>.Configure();
-StringEnumerableTypeHandler.Configure();
+DateOnlyTypeHandler.Configure();
+TimeOnlyTypeHandler.Configure();
 
-var posts = await connection.QueryAsync<Post>("SELECT Id, Title, Content, Date, Authors, Reviews FROM Posts");
+var data = await connection.QueryAsync<Sample>("SELECT * FROM Sample");
+Console.WriteLine(data);
 
-var posts2 = await connection.QueryAsync<Post>("SELECT * FROM Posts WHERE Authors LIKE @authors", new { Authors = $"%Marco%" });
+var d = data.FirstOrDefault();
+var d2 = d with { Id = 2 };
 
-var post = new Post
-{
-    Id = Guid.NewGuid(),
-    Title = "TinyHelpers3",
-    Content = "New Description",
-    Authors = new string[] { "Andrea", "Calogero" },
-    Date = DateTime.UtcNow
-};
+await connection.ExecuteAsync("INSERT INTO Sample(Id, Date, Time) VALUES(@Id, @Date, @Time)", d2);
 
-await connection.ExecuteAsync("INSERT INTO Posts(Id, Title, Content, Date, Authors, Reviews) VALUES(@Id, @Title, @Content, @Date, @Authors, @Reviews)",
-    post);
+internal record class Sample(int Id, DateOnly Date, TimeOnly Time);
+
+//JsonTypeHandler<IList<Review>>.Configure();
+//StringEnumerableTypeHandler.Configure();
+
+//var posts = await connection.QueryAsync<Post>("SELECT Id, Title, Content, Date, Authors, Reviews FROM Posts");
+
+//var posts2 = await connection.QueryAsync<Post>("SELECT * FROM Posts WHERE Authors LIKE @authors", new { Authors = $"%Marco%" });
+
+//var post = new Post
+//{
+//    Id = Guid.NewGuid(),
+//    Title = "TinyHelpers3",
+//    Content = "New Description",
+//    Authors = new string[] { "Andrea", "Calogero" },
+//    Date = DateTime.UtcNow
+//};
+
+//await connection.ExecuteAsync("INSERT INTO Posts(Id, Title, Content, Date, Authors, Reviews) VALUES(@Id, @Title, @Content, @Date, @Authors, @Reviews)",
+//    post);
---
+++ src/TinyHelpers.Dapper/TypeHandlers/DateOnlyTypeHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using Dapper;
+
+namespace TinyHelpers.Dapper.TypeHandlers;
+
+#if NET6_0_OR_GREATER
+public class DateOnlyTypeHandler : SqlMapper.TypeHandler<DateOnly>
+{
+    public override DateOnly Parse(object value)
+        => DateOnly.FromDateTime(Convert.ToDateTime(value));
+
+    public override void SetValue(IDbDataParameter parameter, DateOnly value)
+    {
+        parameter.DbType = DbType.Date;
+        parameter.Value = value.ToDateTime(TimeOnly.MinValue);
+    }
+
+    public static void Configure()
+        => SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
+}
+#endif
