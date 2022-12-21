ed3e69546aae7a3f8e9abe1f4b7494e6847807d5
Defkon1 <defkon1@gmail.com>
2022-12-21 12:43:07 +0100

    [#74] Added XML documentation for classes and public methods

--- src/TinyHelpers/Models/WithIndex.cs
+++ src/TinyHelpers/Models/WithIndex.cs
@@ -1,14 +1,29 @@
 ﻿namespace TinyHelpers;
 
+/// <summary>
+/// Represents a generic object with its relative index.
+/// </summary>
+/// <typeparam name="T">The internal type of the object represented.</typeparam>
 public readonly struct WithIndex<T> where T : class
 {
+    /// <summary>
+    /// Gets or sets the value of the object.
+    /// </summary>
     public T? Value { get; }
 
+    /// <summary>
+    /// Gets or sets the index of the object.
+    /// </summary>
     public int Index { get; }
-
+        
     internal WithIndex(T? value, int index)
         => (Value, Index) = (value, index);
 
+    /// <summary>
+    /// Separates value and index of the instance.
+    /// </summary>
+    /// <param name="value">The value of the instance.</param>
+    /// <param name="index">An <code>int</code> representing the index of the value of the instance.</param>
     public void Deconstruct(out T? value, out int index)
         => (value, index) = (Value, Index);
 }
