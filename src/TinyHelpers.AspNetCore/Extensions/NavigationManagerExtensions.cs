e7ebfe225b8b3600a3dd89c1aadacc112852c8b6
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2023-03-27 15:24:28 +0200

    Update NavigationManagerExtensions.cs

--- src/TinyHelpers.AspNetCore/Extensions/NavigationManagerExtensions.cs
+++ src/TinyHelpers.AspNetCore/Extensions/NavigationManagerExtensions.cs
@@ -13,25 +13,25 @@ public static class NavigationManagerExtensions
         {
             if (typeof(T) == typeof(int) && int.TryParse(valueFromQueryString, out var valueAsInt))
             {
-                value = (T)(object)valueAsInt;
+                value = (T)Convert.ChangeType(valueAsInt, typeof(int));
                 return true;
             }
 
             if (typeof(T) == typeof(string))
             {
-                value = (T)(object)valueFromQueryString.ToString();
+                value = (T)(T)Convert.ChangeType(valueFromQueryString, typeof(string));
                 return true;
             }
 
             if (typeof(T) == typeof(decimal) && decimal.TryParse(valueFromQueryString, out var valueAsDecimal))
             {
-                value = (T)(object)valueAsDecimal;
+                value = (T)Convert.ChangeType(valueAsDecimal, typeof(decimal));
                 return true;
             }
 
             if (typeof(T) == typeof(bool) && bool.TryParse(valueFromQueryString, out var valueAsBool))
             {
-                value = (T)(object)valueAsBool;
+                value = (T)Convert.ChangeType(valueAsBool, typeof(bool));
                 return true;
             }
         }
