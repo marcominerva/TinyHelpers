a3f498086a61eac17141f9f584c905a7689db449
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2023-03-27 12:53:21 +0200

    Add NavigationManagerExtension to retrieve query parameters in a Blazor page

---
+++ src/TinyHelpers.AspNetCore/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace TinyHelpers.AspNetCore.Extensions;
+
+public static class NavigationManagerExtensions
+{
+    public static bool TryGetQueryString<T>(this NavigationManager navManager, string key, out T value)
+    {
+        var uri = navManager.ToAbsoluteUri(navManager.Uri);
+
+        if (QueryHelpers.ParseQuery(uri.Query).TryGetValue(key, out var valueFromQueryString))
+        {
+            if (typeof(T) == typeof(int) && int.TryParse(valueFromQueryString, out var valueAsInt))
+            {
+                value = (T)(object)valueAsInt;
+                return true;
+            }
+
+            if (typeof(T) == typeof(string))
+            {
+                value = (T)(object)valueFromQueryString.ToString();
+                return true;
+            }
+
+            if (typeof(T) == typeof(decimal) && decimal.TryParse(valueFromQueryString, out var valueAsDecimal))
+            {
+                value = (T)(object)valueAsDecimal;
+                return true;
+            }
+
+            if (typeof(T) == typeof(bool) && bool.TryParse(valueFromQueryString, out var valueAsBool))
+            {
+                value = (T)(object)valueAsBool;
+                return true;
+            }
+        }
+
+        value = default;
+        return false;
+    }
+}
