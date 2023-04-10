7db8867cd52fcab7f8ef749d063d4c0645ff5492
Marco Minerva <marco.minerva@gmail.com>
2023-04-10 16:49:23 +0200

    Add IConfigurationExtensions

---
+++ src/TinyHelpers.AspNetCore/Extensions/IConfigurationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace TinyHelpers.AspNetCore.Extensions;
+
+public static class IConfigurationExtensions
+{
+    public static T? GetSection<T>(this IConfiguration configuration, string sectionName) where T : class
+    {
+        var section = configuration.GetSection(sectionName);
+        var settings = section.Get<T>();
+
+        return settings;
+    }
+}
