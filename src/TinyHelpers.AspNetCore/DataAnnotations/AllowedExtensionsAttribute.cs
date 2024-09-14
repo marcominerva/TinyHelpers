fb4ce83260db064b7f8b0eafc2af4aa5c8462866
joegoldman2 <147369450+joegoldman2@users.noreply.github.com>
2024-09-14 14:10:48 +0000

    Reduce allocation in AllowedExtensionsAttribute

--- src/TinyHelpers.AspNetCore/DataAnnotations/AllowedExtensionsAttribute.cs
+++ src/TinyHelpers.AspNetCore/DataAnnotations/AllowedExtensionsAttribute.cs
@@ -7,14 +7,14 @@ namespace TinyHelpers.AspNetCore.DataAnnotations;
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
 public class AllowedExtensionsAttribute(params string[] extensions) : ValidationAttribute("Only files with the following extensions are supported: {0}")
 {
-    private readonly IEnumerable<string> extensions = extensions.Select(e => e.ToLowerInvariant().Replace("*.", string.Empty));
+    private readonly IEnumerable<string> extensions = extensions.Select(e => e.Replace("*.", string.Empty));
 
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
         if (value is IFormFile file)
         {
-            var extension = Path.GetExtension(file.FileName).ToLower()[1..];
-            if (!extensions.Contains(extension))
+            var extension = Path.GetExtension(file.FileName)[1..];
+            if (!extensions.Contains(extension, StringComparer.OrdinalIgnoreCase))
             {
                 return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
             }
