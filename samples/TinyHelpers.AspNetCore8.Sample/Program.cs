ba01e04cc06c0e41c69ecd332c02e4f1da129706
Marco Minerva <marco.minerva@gmail.com>
2024-12-03 11:00:08 +0100

    Introduce TinyHelpers.AspNetCore.Swashbuckle library
    Added new projects `TinyHelpers.AspNetCore.Swashbuckle` and `TinyHelpers.AspNetCore8.Sample` to the solution. Updated solution configuration, replaced `Swashbuckle` with `Microsoft.AspNetCore.OpenApi` in relevant files, and updated `Program.cs` for OpenAPI. Removed `Swagger`-related classes from `TinyHelpers.AspNetCore` and moved them to the new project. Updated `README.md` for migration instructions and new package details. Updated `ServiceCollectionExtensions.cs` and `TinyHelpers.AspNetCore.csproj` for .NET 9.0. Updated version to 4.0. Added GitHub Actions workflow for publishing. Added configuration and development settings files. Introduced new classes and filters for Swagger operations in the new library.
    
    Closes #227

---
+++ samples/TinyHelpers.AspNetCore8.Sample/Program.cs
@@ -0,0 +1,95 @@
+using Microsoft.OpenApi.Models;
+using TinyHelpers.AspNetCore.Extensions;
+using TinyHelpers.AspNetCore.Swagger;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRequestLocalization("it", "en", "de");
+
+builder.Services.AddEndpointsApiExplorer();
+
+// Add parameters that are required for all operations.
+builder.Services.AddSwaggerOperationParameters(options =>
+{
+    options.Parameters.Add(new()
+    {
+        Name = "x-tenant",
+        In = ParameterLocation.Header,
+        Required = true,
+        Schema = OpenApiSchemaHelper.CreateStringSchema()
+    });
+
+    options.Parameters.Add(new()
+    {
+        Name = "x-environment",
+        In = ParameterLocation.Header,
+        Schema = OpenApiSchemaHelper.CreateSchema<Environment>(Environment.Production)
+    });
+
+    options.Parameters.Add(new()
+    {
+        Name = "code",
+        In = ParameterLocation.Query,
+        Schema = OpenApiSchemaHelper.CreateSchema<Guid>("string", "uuid")
+    });
+
+    options.Parameters.Add(new()
+    {
+        Name = "version",
+        In = ParameterLocation.Query,
+        Schema = OpenApiSchemaHelper.CreateSchema<int>("integer", "int32", 1)
+    });
+});
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddAcceptLanguageHeader();
+    options.AddDefaultResponse();
+
+    // Enable Swagger integration for custom parameters.
+    options.AddOperationParameters();
+});
+
+// Add default problem details and exception handler.
+builder.Services.AddDefaultProblemDetails();
+builder.Services.AddDefaultExceptionHandler();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
+app.UseStatusCodePages();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+app.UseRequestLocalization();
+
+app.MapGet("/api/sample", () =>
+{
+    var language = Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName;
+    return TypedResults.NoContent();
+})
+.WithOpenApi();
+
+app.MapPost("/api/exception", () =>
+{
+    throw new Exception("This is an exception", innerException: new HttpRequestException("This is an inner exception"));
+})
+.WithOpenApi();
+
+app.Run();
+
+public enum Environment
+{
+    Development,
+    Staging,
+    Production
+}
