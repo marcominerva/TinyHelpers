9ef0b2b5f3ccd1f5143f194a128300159e1dcb37
Francesco Del Re <francesco.delre.87@gmail.com>
2024-12-19 00:55:36 +0100

    feat: Add GetAttribute extension method for retrieving attributes from Assembly

---
+++ tests/TinyHelpers.Tests/Extensions/AssemblyExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using TinyHelpers.Extensions;
+
+namespace TinyHelpers.Tests.Extensions;
+
+public class AssemblyExtensionsTests
+{
+    // Custom test class with an Obsolete attribute
+    [Obsolete("This class is obsolete.")]
+    public class MyClass
+    {
+        public void MyMethod()
+        {
+        }
+    }
+
+    [Fact]
+    public void GetAttribute_ReturnsCorrectValue_WhenObsoleteAttributeIsPresent()
+    {
+        // Arrange
+        var assembly = Assembly.GetAssembly(typeof(MyClass));
+
+        // Act
+        var attribute = assembly.GetAttribute<ObsoleteAttribute>(attr => attr.Message);
+
+        // Assert
+        Assert.NotNull(attribute);
+        Assert.Equal("This class is obsolete.", attribute);
+    }
+}
