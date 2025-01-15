6c55ce8a8cd99922ee27c4f57773e2f9fdec788f
Marco Minerva <marco.minerva@gmail.com>
2025-01-15 12:43:43 +0100

    Add methods to retrieve entity types in ModelBuilderExtensions
    Added a generic method `GetEntityTypes<TType>` to the `ModelBuilderExtensions` class, which retrieves entity types based on a specified type parameter. This method internally calls another new method `GetEntityTypes` that takes a `Type` parameter and returns entity types from the model that are assignable from the specified base type. These enhancements provide a more flexible way to query entity types in the model.

--- src/TinyHelpers.EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
+++ src/TinyHelpers.EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
@@ -33,6 +33,9 @@ public static class ModelBuilderExtensions
         }
     }
 
+    public static IEnumerable<Type> GetEntityTypes<TType>(this ModelBuilder modelBuilder)
+        => GetEntityTypes(modelBuilder, typeof(TType));
+
     public static IEnumerable<Type> GetEntityTypes(this ModelBuilder modelBuilder, Type baseType)
     {
         var entityTypes = modelBuilder.Model.GetEntityTypes()
